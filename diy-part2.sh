47ac3e02d3710d3ac8f5e70c569f9899b3d6fdf1
Baka <29482388+BakaAwt@users.noreply.github.com>
2022-01-20 22:32:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,8 +11,10 @@
 #
 
 # BCM57810s kernel patch
+wget -O target/linux/x86/patches-5.4/600-bnx2x-warpcore-8727-2g5.patch https://raw.githubusercontent.com/JAMESMTL/snippets/master/bnx2x/patches/bnx2x_warpcore_8727_2_5g_sgmii_txfault.patch
 wget -O target/linux/x86/patches-5.10/600-bnx2x-warpcore-8727-2g5.patch https://raw.githubusercontent.com/JAMESMTL/snippets/master/bnx2x/patches/bnx2x_warpcore_8727_2_5g_sgmii_txfault.patch
-# wget -O target/linux/x86/patches-5.10.91/600-bnx2x-warpcore-8727-2g5.patch https://raw.githubusercontent.com/JAMESMTL/snippets/master/bnx2x/patches/bnx2x_warpcore_8727_2_5g_sgmii_txfault.patch
+wget -O target/linux/x86/patches-5.15/600-bnx2x-warpcore-8727-2g5.patch https://raw.githubusercontent.com/JAMESMTL/snippets/master/bnx2x/patches/bnx2x_warpcore_8727_2_5g_sgmii_txfault.patch
+# wget -O target/linux/x86/patches-5.10/600-bnx2x-warpcore-8727-2g5.patch https://raw.githubusercontent.com/JAMESMTL/snippets/master/bnx2x/patches/bnx2x_warpcore_8727_2_5g_sgmii_txfault.patch
 
 # Modify default IP
 sed -i 's/192.168.1.1/172.16.0.1/g' package/base-files/files/bin/config_generate
